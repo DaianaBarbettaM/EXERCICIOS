b7f55231a63ae33e282d37f31f283f41af5a391a
DaianaBarbettaM <38558628+DaianaBarbettaM@users.noreply.github.com>
2018-04-27 10:32:08 -0300

    Adicionado o menu da questão 06

--- Questao06.java
+++ Questao06.java
@@ -18,20 +18,22 @@ public class Questao06{
     
              
 While pedidos = JOptionPane.showInputDialog(null,   "NOME                                                         VALOR"+
-                                                                "\nBolo Brigadeiro...............................................R$ 29.50"+
-                                                                "\nBolo Floresta Negra...........................................R$  2.00"+
-                                                                "\nBolo Leite com nutella........................................R$ 29.23"+
-                                                                "\nBolo Mousse de Chocolate......................................R$  7.10"+
-                                                                "\nBolo Nega Maluca..............................................R$ 19.33"+
-                                                                "\nBomba de Creme................................................R$ 17.71"+
-                                                                "\nBomba de Morango..............................................R$  4.82"+
-                                                                "\nSanduiche File-Mignon com fritas e cheddar....................R$ 21.16"+
-                                                                "\nSanduiche Hamburger com queijos champignon e rucula...........R$ 12.70"+
-                                                                "\nSanduiche Provolone com Salame................................R$ 19.70"+
-                                                                "\nSanduiche Vegetariano de Berinjela............................R$ 28.22"+
-                                                                "\nPizza Calabresa...............................................R$  8.98"+
-                                                                "\nPizza Napolitana..............................................R$  0,42"+
-                                                                "\nPizza Peruana.................................................R$ 18.36"+
-                                                                "\nPizza Potuguesa...............................................R$ 27.50"+
-                                                                "\nInforme seu primeiro pedido:   ");
+                                                                "\n 1  Bolo Brigadeiro...............................................R$ 29.50"+
+                                                                "\n 2  Bolo Floresta Negra...........................................R$  2.00"+
+                                                                "\n 3  Bolo Leite com nutella........................................R$ 29.23"+
+                                                                "\n 4  Bolo Mousse de Chocolate......................................R$  7.10"+
+                                                                "\n 5  Bolo Nega Maluca..............................................R$ 19.33"+
+                                                                "\n 6  Bomba de Creme................................................R$ 17.71"+
+                                                                "\n 7  Bomba de Morango..............................................R$  4.82"+
+                                                                "\n 8  Sanduiche File-Mignon com fritas e cheddar....................R$ 21.16"+
+                                                                "\n 9  Sanduiche Hamburger com queijos champignon e rucula...........R$ 12.70"+
+                                                                "\n 10 Sanduiche Provolone com Salame................................R$ 19.70"+
+                                                                "\n 11 Sanduiche Vegetariano de Berinjela............................R$ 28.22"+
+                                                                "\n 12 Pizza Calabresa...............................................R$  8.98"+
+                                                                "\n 13 Pizza Napolitana..............................................R$  0,42"+
+                                                                "\n 14 Pizza Peruana.................................................R$ 18.36"+
+                                                                "\n 15 Pizza Potuguesa...............................................R$ 27.50"+
+                                                                "\n 16 Sair " +
+                                                                "Informe seu primeiro pedido:   ");
+            
             
